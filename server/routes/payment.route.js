f108f727b10f48ea6e1afd5492dbbd7b6e8c57b0
AB <136963258+abDMA@users.noreply.github.com>
2024-12-10 15:12:27 +0100

    Delete server/routes/payment.route.js

--- server/routes/payment.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { checkOutSuccess, createCheckoutSession } from "../controllers/payment.controller.js"
-import { verifyToken } from "../middleware/verifyToken.js"
-const router = express.Router()
-router.post('/create-checkout-session',verifyToken,createCheckoutSession)
-router.post('/checkout-success',verifyToken,checkOutSuccess)
-export default router
